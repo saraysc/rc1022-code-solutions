f9ac30df8a5513b3806878600b139d87036907ae
Sara Choi <sarachoi10@hotmail.com>
2022-10-17 18:34:18 +0000

    Completed javascript-operators-and-expressions.

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,46 @@
+var width = 9;
+var height = 10;
+var area = width * height;
+
+console.log('value of area:', area);
+console.log('type of area: ', typeof area);
+
+var bill = 10;
+var payment = 8;
+var change = bill - payment;
+
+console.log('value of change:', change);
+console.log('type of change:', typeof change);
+
+var quizzes = 7;
+var midterm = 9;
+var final = 10;
+var grade = (quizzes + midterm + final) / 3;
+
+console.log('value of grade:', grade);
+console.log('type of grade:', typeof grade);
+
+var firstName = 'Sara';
+var lastName = 'Choi';
+var fullName = `${firstName} ${lastName}`;
+
+console.log('value of fullName:', fullName);
+console.log('type of fullName:', typeof fullName);
+
+var ph = 6;
+var isAcidic = ph < 7;
+
+console.log('value of isAcidic:', isAcidic);
+console.log('type of isAcidic:', typeof isAcidic);
+
+var headCount = 170;
+var isSparta = headCount === 300;
+
+console.log('value of isSparta:', isSparta);
+console.log('type of isSparta:', typeof isSparta);
+
+var motto = 'Sara Choi';
+motto += ' is the GOAT';
+
+console.log('value of motto:', motto);
+console.log('type of motto:', typeof motto);
