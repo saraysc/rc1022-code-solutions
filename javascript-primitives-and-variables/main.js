136212ac4006f323899abbcb25cf366d8b0f33d9
Sara Choi <sarachoi10@hotmail.com>
2022-10-17 17:03:31 +0000

    Completed javascript-primitives-and-variables.

--- javascript-primitives-and-variables/main.js
+++ javascript-primitives-and-variables/main.js
@@ -4,18 +4,18 @@ var isCool = true;
 
 var totalPets = 0;
 
-console.log(fullName);
-console.log(isCool);
-console.log(totalPets);
+console.log('value of fullName: ' + fullName);
+console.log('value of isCool: ' + isCool);
+console.log('value of totalPets: ' + totalPets);
 
-console.log(typeof fullName);
-console.log(typeof isCool);
-console.log(typeof totalPets);
+console.log('typeof fullName: ' + typeof fullName);
+console.log('typeof isCool: ' + typeof isCool);
+console.log('typeof totalPets: ' + typeof totalPets);
 
 var unknown = null;
-console.log(unknown);
-console.log(typeof unknown);
+console.log('value of unknown: ' + unknown);
+console.log('typeof unknown: ' + typeof unknown);
 
 var never;
-console.log(never);
-console.log(typeof never);
+console.log('value of never: ' + never);
+console.log('typeof never: ' + typeof never);
