fe797897d74ba23cdafa95467affb6e31ce9e9fb
Sara Choi <sarachoi10@hotmail.com>
2022-10-17 16:55:27 +0000

    Completed javascript-primitives-and-variables exercise.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,21 @@
+var fullName = 'Sara Choi';
+
+var isCool = true;
+
+var totalPets = 0;
+
+console.log(fullName);
+console.log(isCool);
+console.log(totalPets);
+
+console.log(typeof fullName);
+console.log(typeof isCool);
+console.log(typeof totalPets);
+
+var unknown = null;
+console.log(unknown);
+console.log(typeof unknown);
+
+var never;
+console.log(never);
+console.log(typeof never);
