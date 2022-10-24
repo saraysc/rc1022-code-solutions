9af4003dfa1f6aed25b31bf0c5b752544321e166
Sara Choi <sarachoi10@hotmail.com>
2022-10-24 17:12:41 +0000

    Completed dom-querying exercise.

---
+++ dom-querying/main.js
@@ -0,0 +1,5 @@
+console.dir(document.querySelector('h1'));
+console.dir(document.querySelector('#explanation'));
+console.dir(document.querySelector('.hint'));
+console.dir(document.querySelectorAll('p'));
+console.dir(document.querySelectorAll('.example-link'));
