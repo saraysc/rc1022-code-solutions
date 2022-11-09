3eb20cf3d4d4fdc38b7da76aedcbb6d0bc84256a
Sara Choi <sarachoi10@hotmail.com>
2022-11-09 18:18:53 +0000

    Reviewed javascript-timers

--- javascript-timers/interval.js
+++ javascript-timers/interval.js
@@ -4,8 +4,8 @@ function timeout() {
   $countdown.textContent -= 1;
   if ($countdown.textContent === '0') {
     $countdown.textContent = '~Earth Beeeeloooowwwww Us~';
+    clearInterval(i);
   }
 }
 
 var i = setInterval(timeout, 1000);
-setTimeout(function () { clearInterval(i); }, 4000);
