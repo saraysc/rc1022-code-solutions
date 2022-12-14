bab9539762ce4a2de668efdb91c2b9e765b50d52
Sara Choi <sarachoi10@hotmail.com>
2022-12-14 06:26:38 +0000

    updates 1

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -15,39 +15,43 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.get('/api/notes/:id', (req, res) => {
-  if (!Number.isInteger(req.params.id) || req.params.id<0){
-    res.status(400).json(`Error: Id must be a positive integer`);
-  } else if (data.notes[req.params.id] ===undefined){
-    res.status(404).json(`Error: Cannot find note with id ${req.params.id} `);
+  if (isNaN(req.params.id) || Number(req.params.id) <= 0){
+    res.status(400).send(`Error: Id must be a positive integer`);
+  } else {
+    if(!data.notes[req.params.id]){
+    res.status(404).send(`Error: Cannot find note with id ${req.params.id} `);
   } else{
-    res.status(200).json(data.notes[req.params.id]);
+    res.status(200).send(data.notes[req.params.id]);
   }
+}
 });
 
 app.post('/api/notes', (req, res) => {
   if(!req.body.content){
-    res.status(400).json(`Error: Content is a required field`);
+    res.status(400).send(`Error: Content is a required field`);
   } else{
     data.notes[data.nextId] = req.body;
     data.notes[data.nextId].id = data.nextId;
-    res.status(201).json(data.notes[data.nextId]);
+    res.status(201).send(data.notes[data.nextId]);
     data.nextId++;
     writeFile(data,res,'post');
 }})
 
 app.delete('/api/notes/:id', (req, res) => {
-  if (!req.params.id){
+  if (isNaN(req.params.id) || Number(req.params.id) <= 0){
     res.status(400).send("Id number is undefined")
-  } else if (!data.notes[req.params.id]){
+  } else{
+    if (!data.notes[req.params.id]){
     res.status(404).send("Id number does not exist")
   } else if (data.notes[req.params.id]){
     delete data.notes[req.params.id];
     res.sendStatus(204)
     writeFile(data,res,'delete')
+  }
 }});
 
 app.put('/api/notes/:id', (req,res)=>{
-  if (!Number.isInteger(req.params.id) || req.params.id < 0) {
+  if (isNaN(req.params.id) || Number(req.params.id) <= 0) {
     res.status(400).send(`Error: id must be a positive integer`)
   } else if (!data.notes[req.params.id]) {
     res.status(404).send(`Error: cannot find note with id ${req.params.id}`)
