1ca06fc0e7c85cb574f6e301fd7dff79e906742a
Sara Choi <sarachoi10@hotmail.com>
2022-12-15 21:54:37 +0000

    Completed react-stopwatch-component exercise.

---
+++ react-stopwatch-component/src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import * as ReactDOM from 'react-dom';
+import Stopwatch from './stopwatch';
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(<Stopwatch />);
