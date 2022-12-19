e73d243415dd68af78aa86addad3d4ea33902c46
Sara Choi <sarachoi10@hotmail.com>
2022-12-19 07:18:45 +0000

    Update1

---
+++ react-accordion-component/src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import * as ReactDOM from 'react-dom';
+import Accordion from './accordion.jsx';
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(<Accordion />);
