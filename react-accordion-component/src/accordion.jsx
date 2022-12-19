4f6c342e1b9e165eda5e6b0696865d5531d5d79d
Sara Choi <sarachoi10@hotmail.com>
2022-12-19 23:14:54 +0000

    updates2

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -8,7 +8,28 @@ export default class Accordion extends React.Component {
   }
 
   handleClick(event) {
+    this.setState({ clickedTopicId: event.target.dataset.topicId });
+    if (event.target.dataset.topicId === this.state.clickedTopicId) {
+      this.setState({ clickedTopicId: null });
+    }
+  }
 
+  render() {
+    const topics = this.props.topics;
+    const listItems = topics.map(topic => {
+      const toggle = Number(this.state.clickedTopicId) === topic.id ? '' : 'hidden';
+      return <li key={topic.id}>
+        <div data-topic-id={topic.id} className='topic' onClick={this.openTopic}>
+          {topic.name}
+        </div>
+        <div className={`details ${toggle}`}>{topic.details}</div>
+      </li>;
+    });
+    return (
+      <ul>
+        {listItems}
+      </ul>
+    );
   }
 }
 
