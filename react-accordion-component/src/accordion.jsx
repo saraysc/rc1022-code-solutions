b666b46dec9d47a455b669bf1ba9f0d5fbb5cc9a
Sara Choi <sarachoi10@hotmail.com>
2022-12-20 07:40:45 +0000

    Completed react-accordion-component exercise

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -3,36 +3,42 @@ import React from 'react';
 export default class Accordion extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { clickedTopicId: null };
+    this.state = ({
+      tab: null
+    });
     this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick(event) {
-    this.setState({ clickedTopicId: event.target.dataset.topicId });
-    if (event.target.dataset.topicId === this.state.clickedTopicId) {
-      this.setState({ clickedTopicId: null });
+  handleClick(id, event) {
+    if (id !== this.state.tab) {
+      this.setState({
+        tab: id
+      });
+    } else {
+      this.setState({
+        tab: null
+      });
     }
   }
 
   render() {
     const topics = this.props.topics;
-    const listItems = topics.map(topic => {
-      const toggle = Number(this.state.clickedTopicId) === topic.id ? '' : 'hidden';
+    const listItem = topics.map(topic => {
+      const toggle = this.state.tab === topic.id ? '' : 'hidden';
       return <li key={topic.id}>
-        <div data-topic-id={topic.id} className='topic' onClick={this.openTopic}>
-          {topic.name}
+        <div data-id={topic.id} className='link' onClick={event => this.handleClick(topic.id, event)}>
+          {topic.title}
         </div>
-        <div className={`details ${toggle}`}>{topic.details}</div>
+        <div data-id={topic.id} className = {`text ${toggle}`}>
+          {topic.text}
+        </div>
       </li>;
     });
     return (
-      <ul>
-        {listItems}
-      </ul>
+      <ul>{listItem}</ul>
     );
   }
 }
-
 export const elements = [
   { id: 1, title: 'Hypertext Markup Language', text: 'Hypertext MarkupLanguage (HTML) is the standard markup language for creating web pages and web applications. With Cascading Style Sheets (CSS) and JavaScript, it forms a triad of cornerstone technologies for the World Wide Web' },
   { id: 2, title: 'Cascading Style Sheets', text: 'Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup language like HTML. CSS is cornerstone technology of the World Wide Web alongside HTML and JavaScript.' },
