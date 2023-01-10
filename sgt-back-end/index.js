27854a61f1096ee43f8c72bfa1d53cd256f6d503
Sara Choi <sarachoi10@hotmail.com>
2023-01-10 00:19:47 +0000

    First Update

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -1,8 +1,8 @@
+const pg = require('pg');
 const express = require('express');
 const app = express();
-app.use(express.json());
-
-const pg = require('pg');
+const jsonMiddleware = express.json();
+app.use(jsonMiddleware);
 
 // only create ONE pool for your whole server
 const db = new pg.Pool({
@@ -12,83 +12,26 @@ const db = new pg.Pool({
   }
 });
 
-const errorMessage = {
-  requiredField: { error: 'name, course, and score are required field' },
-  notInteger: { error: 'score should be an integer from 0 to 100' },
-  invalidGradeId: { error: 'invalid gradeId' },
-  inexistentGradeId: {error: `cannot find note with id ${req.params.id}`
-},
-  unexpected: {
-    error: 'The server encountered an unexpected condition which prevented it from fulfilling the request.'
-  }
-};
-
-app.use(express.json());
-
-app.get('/api/grades', (req, res) => {
+app.get('/api/grades', (req, res, next) => {
   const sql = `
-  SELECT *
-  FROM "grades"
+  select *
+  from "grades"
   `;
   db.query(sql)
     .then(result => {
-      const grade = result.rows;
-      res.status(200).send(grade);
-    }).catch(err => {
-      console.error(err);
-      res.status(500).send(errorMessage.unexpected);
-    });
+      res.status(201).json(result.rows);
+    })
+    .catch(err => next(err));
 });
 
-app.post('/api/grades', (req, res) => {
-  if (!req.body.name || !req.body.course || !req.body.score) {
-    res.status(400).send(errorMessage.requiredField);
-  } else if (!(req.body.score >= 0 && req.body.score <= 100) || isNaN(req.body.score)) {
-    res.status(400).send(errorMessage.notInteger);
-  } else {
-    const sql = `
-  insert into "grades" ("name", "course","score")
-  values($1,$2,$3)
-  returning *
+app.post('/api/grades', (req, res, next) => {
+  const sql = `
+  insert *
+  from "grades"
   `;
-    const params = [req.body.name, req.body.course, req.body.score];
-    db.query(sql, params)
-      .then(result => {
-        const grade = result.rows[0];
-        res.status(201).send(grade);
-      }).catch(err => {
-        console.error(err);
-        res.status(500).send(errorMessage.unexpected);
-      });
-  }
-
-});
-app.put('/api/notes/:gradeId', (req, res) => {
-  if (isNaN(req.params.gradeId)) {
-    if (Number.isInteger(req.params.gradeId) && req.params.gradeId > 0) {
-      res.status(400).send(errorMessage.invalidGradeId)
-    } else if (!data.notes[req.params.id]) {
-      res.status(404).send(errorMessage.inexistentGradeId)
-    } else{
-      const sql = `
-      insert into "grades" ("name", "course","score")
-      values($1,$2,$3)
-      returning *
-  `;
-      const params = [req.body.name, req.body.course, req.body.score];
-      db.query(sql, params)
-        .then(result => {
-          const grade = result.rows[0];
-          res.status(201).send(grade);
-        }).catch(err => {
-          console.error(err);
-          res.status(500).send(errorMessage.unexpected);
-        });
-    }
-}
-}
-
-app.listen(3000, () => {
-  // eslint-disable-next-line no-console
-  console.log('Express Server listening on port 3000');
+  db.query(sql)
+    .then(result => {
+      res.status(201).json(result.rows);
+    })
+    .catch(err => next(err));
 });
